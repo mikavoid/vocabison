2b783727908833bb5ce6ddb7976f63f3efd0005f
mikavoid <mikavoid@gmail.com>
2016-12-21 11:02:05 +0100

    Responsive simple header added

--- vocabison/imports/ui/components/Header/Header.jsx
+++ vocabison/imports/ui/components/Header/Header.jsx
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            mainTitle : 'VOCABISON',
+            mobileMenuIsOpen : false
+        }
+    }
+
+    toggleMobileHeader () {
+        console.log(!this.state.mobileMenuIsOpen);
+        this.setState({
+            mobileMenuIsOpen : !this.state.mobileMenuIsOpen
+        });
+    }
+
     render () {
         return (
             <header id="header" className="header">
                 <div className="wrapper">
                     <div className="header__branding">
                         <a className="logo" href="#"><img src="images/logo-vocabison.png" alt="Vocabison's logo" title="Vocabison's logo"/></a>
-                        <h1 className="mainTitle"><a href="#">VOCABISON</a></h1>
+                        <h1 className="mainTitle"><a href="#">{this.state.mainTitle}</a></h1>
                     </div>
                     <div className="header__body">
-                        <a href="#" className="button loginButton">Login</a>
-                        <a href="#" className="button focus loginButton">Register</a>
-                        <a href="#" className="burger"><span className="fa fa-bars"></span></a>
+                        <ul  className="header__body__buttons">
+                            <li><a href="#" className="button loginButton">Login</a></li>
+                            <li><a href="#" className="button focus loginButton">Register</a></li>
+                        </ul>
                     </div>
+
                 </div>
             </header>
         )
